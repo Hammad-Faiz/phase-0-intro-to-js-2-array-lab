d28af71400cea14ed31c9936d53b984861c5d698
Hammad <hammadfaiz172@gmail.com>
2022-05-28 12:27:46 -0400

    commit tree 0e0d4216d4f39bc55ef94708936083afaf3834b7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const cat = [
+        ...cats.slice(3,3),
+        ...cats.slice(1),
+    ];
+}
